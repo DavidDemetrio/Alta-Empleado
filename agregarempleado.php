97ae8ef77fddae29bb0d12534ac3755dd1086b23
dlopez <davidlopez.paz26@outlook.es>
2021-04-06 13:19:29 -0500

    modificación de la lectura del código más comoda en agregarempleado.php

--- agregarempleado.php
+++ agregarempleado.php
@@ -1,23 +1,22 @@
 <?php
     include "basededatos.php";
 
-    if(!empty($_POST['nombre_empleado']) && !empty($_POST['empleado_edad'])){
-        $nombre = $_POST['nombre_empleado'];
-        $edad = $_POST['empleado_edad'];
+    if(!empty($_POST['nombre_empleado']) && !empty($_POST['empleado_edad']))
+            {
+                $nombre = $_POST['nombre_empleado'];
+                $edad = $_POST['empleado_edad'];
 
-        $sql = "INSERT INTO empleado (nombre,edad)  
-                  VALUES ('$nombre', '$edad')";
-        $exito = mysqli_query($conexion,$sql);
+                $sql = "INSERT INTO empleado (nombre,edad)  
+                        VALUES ('$nombre', '$edad')";
+                
+                $exito = mysqli_query($conexion,$sql);
 
-        if($exito){
-            header('location: listaempleado.php');
-        }
-        else{
-            header('location:index.php');
-        }
-
-    }
-    else{
-        header('location:index.php');
-    }
+                if($exito)
+                    header('location: listaempleado.php');         
+                else
+                    header('location:index.php');
+                    
+            }
+    else
+        header('location:index.php');       
 ?>
