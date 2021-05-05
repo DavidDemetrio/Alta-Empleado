71d4c47943c08ddea7111e6678032e157eb13a2b
David López <81669207+DavidDemetrio@users.noreply.github.com>
2021-05-05 11:17:11 -0500

    Update listaempleado.php

--- listaempleado.php
+++ listaempleado.php
@@ -1,9 +1,9 @@
 <?php
     include "basededatos.php";
 
-    $sql = "SELECT id,nombre,edad FROM empleado";
-    $resultado = mysqli_query($conexion, $sql);  //Comprobamos la conexion a la db y la ejecución correcta del query
-    mysqli_close($conexion);
+    $sql = "SELECT id,nombre,edad FROM empleado"; //query a ejecutar.
+    $resultado = mysqli_query($conexion, $sql);  //Comprobamos la correcta conexión a la db y la ejecución del query.
+    mysqli_close($conexion); //cerramos la conexión a la db.
 ?>
 
 <!DOCTYPE html>
@@ -19,19 +19,25 @@
 <body>
     <h1 style="color:orange">Lista Empleados</h1>
 
+    <!--Lista Empleados-->
     <ul>
         <?php
-            while ($registro = mysqli_fetch_assoc($resultado)) //imprime en forma de lista los registros llevados hasta el momento
+            /**Con el ciclo While mostrará todas las filas insertadas de los Empleados hasta el momento,
+               en la variable $registro se almacenará cada fila en forma de array asociativo*/
+            while ($registro = mysqli_fetch_assoc($resultado))
                 {
-                    echo '<li>' . $registro['nombre'] . '&nbsp(' . $registro['edad'] . '&nbspaños)&nbsp';
-                    echo '<a href= "modificarempleado.php?empleadoid=' . $registro['id']. 
-                         '">Modificar</a>&nbsp&nbsp';
-                    echo '<a href= "eliminarempleado.php?empleadoid=' . $registro['id'] . 
-                         '">Eliminar</a></li>';
+                    echo '<li>' . $registro['nombre'] . '&nbsp(' . $registro['edad'] . '&nbspaños)&nbsp'; //imprime el valor asociado a 'nombre' y 'edad'
+                    echo '<a href= "modificarempleado.php?empleadoid=' . $registro['id'].  //Enlaza a la página modificarempleado correspondiente a su valor asociado 'id'
+                         '">Modificar</a>&nbsp&nbsp';                                      //mediante la palabra modificar.
+                    echo '<a href= "eliminarempleado.php?empleadoid=' . $registro['id'] .  //Enlza a la página eliminarempleado correspondiente a su valor asociado en 'id'
+                         '">Eliminar</a></li>';                                            //mediante la palabra eliminar.            
                 }
         ?>
+        <!--Regresar a la página principal: Alta Empleado-->
         <a href="index.php"><img style ="margin-top:15px" src= "flecha.png" width="20px" alt="Regresar" title="Regresar a Alta Empleado"></a>
     </ul>
+    <!--Fin Lista Empleados-->
+    
 </body>
 
 </html>
